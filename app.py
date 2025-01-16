0b5788a9375f61c30794f032678d43e517010da8
Andrew Weisman <andrew.weisman@nih.gov>
2025-01-16 10:58:42 -0500

    Remove README.md rendering since NIDAP doesn't copy it over to a published application

--- app.py
+++ app.py
@@ -22,32 +22,16 @@ def main():
         }
     )
 
-    # Set the page layout to wide.
-    st.set_page_config(layout='wide')
-
     # If a new Streamlit session has been connected, display a warning message.
     if 'session_has_been_connected' not in st.session_state:
         st.warning(f'Connecting to a new Streamlit session on {datetime.now(pytz.timezone("US/Eastern")).strftime("%Y-%m-%d %I:%M:%S %p %Z")}.')
         st.session_state['session_has_been_connected'] = True
     
-    # In the first of two main columns...
-    main_columns = st.columns([1/3, 2/3])
-    with main_columns[0]:
+    # Display a header.
+    st.title(pg.title)
 
-        st.write(os.listdir())
-
-        # Display README.md as markdown in this app.
-        with open(os.path.join(os.getcwd(), 'README.md'), 'r') as f:
-            st.markdown(f.read())
-
-    # In the second of two main columns...
-    with main_columns[1]:
-
-        # Display a header.
-        st.subheader(pg.title)
-
-        # Display the page.
-        pg.run()
+    # Display the page.
+    pg.run()
 
 
 # Run the main function.
